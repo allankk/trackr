3d29ae16f2dde7d4b3add2eaffd815ed557f4465
allankk <allanolen@gmail.com>
2024-08-01 22:29:53 +0300

    front routing 1

---
+++ backend/src/main/java/com/allank/trackr/controller/FrontendController.java
@@ -0,0 +1,14 @@
+package com.allank.trackr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FrontendController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        // Forward to `index.html` so frontend can handle routing.
+        return "forward:/index.html";
+    }
+}
