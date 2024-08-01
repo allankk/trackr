3b84fc282e9af5e2d5ef3a343a19e3d6585295fb
allankk <allanolen@gmail.com>
2024-08-01 22:48:07 +0300

    frontroute 2

--- backend/src/main/java/com/allank/trackr/controller/FrontendController.java
+++ backend/src/main/java/com/allank/trackr/controller/FrontendController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = "/{path:[^\\.]*}")
+    @RequestMapping(value = "/{path:^(?!api).*}")
     public String redirect() {
         // Forward to `index.html` so frontend can handle routing.
         return "forward:/index.html";
